cbb4a63afa0ce4f84170fdb0fa1b5fa29032cefe
hobot <kurochkin-va@yandex.ru>
2022-01-21 17:41:11 +0300

    With sqlite db for users

--- auth/security.py
+++ auth/security.py
@@ -1,6 +1,7 @@
 import hashlib
 import json
 import jwt
+from auth import dao
 
 key = "This is secret key"
 
@@ -10,30 +11,24 @@ class CustomSecurity:
         pass_hash = hashlib.sha256(password.encode())
         password_hash_to_db = pass_hash.hexdigest()
 
-        new_data = {"login": login, "password": password_hash_to_db}
-
-        with open("database.json") as all_users_in_json:
-            all_data_about_users = json.load(all_users_in_json)
-            all_data_about_users[login] = new_data
-
-        with open("database.json", "w") as database:
-            json.dump(all_data_about_users, database)
+        newclass = dao.DAO()
+        new_user = newclass.create_user_in_db(login, password_hash_to_db)
+        return new_user
 
     def check_user(self, login, password):
-        with open("database.json") as all_users_in_json:
-            all_data_about_users = json.load(all_users_in_json)
+        pass_hash = hashlib.sha256(password.encode())
+        password_hash_to_db = pass_hash.hexdigest()
 
-            pass_hash = hashlib.sha256(password.encode())
-            password_hash_to_db = pass_hash.hexdigest()
+        newclass = dao.DAO()
+        user_data = newclass.extract_userdata_from_db(login)
 
-            all_data_one_user = all_data_about_users[login]
 
-            if password_hash_to_db == all_data_one_user["password"]:
-                jwtmaker = JwtWorker()
-                user_jwt = jwtmaker.generate_jwt(login)
-                return {"status": "OK", "jwt": user_jwt}
-            else:
-                return f"Bad password"
+        if password_hash_to_db == user_data["hashed_pass"]:
+            jwtmaker = JwtWorker()
+            user_jwt = jwtmaker.generate_jwt(login)
+            return {"status": "OK", "jwt": user_jwt}
+        else:
+            return f"Bad password"
 
 class JwtWorker:
     def generate_jwt(self, login):
